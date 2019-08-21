de2deef6fa4049a417e1e93089716185613262c9
joaopauloorg <joaopaulo@orgsystem.com.br>
2019-08-21 00:14:17 -0300

    depedencias e travis

---
+++ meu-primeiro-projeto/README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.org/joaopaulo1023/javaSpringBoot.svg?branch=master)](https://travis-ci.org/joaopaulo1023/javaSpringBoot)
+
+# meu-primeiro-projeto
+
+Meu primeiro projeto do curso de Spring Boot
---
+++ meu-primeiro-projeto/src/main/java/br/com/mrl/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,14 @@
+package br.com.mrl.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		System.out.println("Meu primeiro projeto...");
+	}
+
+}
